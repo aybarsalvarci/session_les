cd152102083593403defadf6ef9bf488009ed23c
aybarsalvarci <aybarsalvarci44@gmail.com>
2021-01-19 21:57:04 +0300

    Title alanı güncellendi.

--- detay.php
+++ detay.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detaylar</title>
+</head>
+<body>
+    
+
+
+
+
 <?php 
 
     require_once 'baglan.php';
@@ -75,3 +88,11 @@
 
 
 ?>
+
+
+
+
+
+
+</body>
+</html>
